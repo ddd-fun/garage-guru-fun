ebffdabb2fe9c5b0637b2d7167b6e42e6c086e8e
ddd-fun <laserdev@gmail.com>
2016-01-28 23:50:51 +0100

    regexp command line

--- src/main/scala/org/garage/guru/Main.scala
+++ src/main/scala/org/garage/guru/Main.scala
@@ -8,24 +8,18 @@ object Main  {
   def main(args: Array[String]) {
 
 
+    import org.garage.guru.infrastructure.InMemoryRepository
+    val repository = new InMemoryRepository
+    repository.addFreeLot(FreeParkingLot(LotLocation("A", "1")))
 
     for (ln <- io.Source.stdin.getLines){
-      if("exit" == ln) scala.util.control.Breaks.break()
-      println(ln)
+      "(\\w*)\\s*(\\w*)".r.findFirstMatchIn(ln).map(_.subgroups.filter(!_.isEmpty)) match {
+        case Some(List("exit")) => scala.util.control.Breaks.break();
+        case Some(List("park", v)) => println( ParkingServiceObj.park(Vehicle(v))(repository) );
+        case Some(List("clean", v)) => println( ParkingServiceObj.cleanParkingLot(Vehicle(v))(repository) )
+        case _=> println("unknown command "+ln)
+      }
     }
-
-
-
-//    import org.garage.guru.infrastructure.InMemoryRepository
-//
-//    val repository = new InMemoryRepository
-//    repository.addFreeLot(FreeParkingLot(LotLocation("A", "1")))
-//
-//    println( ParkingServiceObj.park(Vehicle("AA123"))(repository) )
-//
-//    println( ParkingServiceObj.cleanParkingLot(Vehicle("AA123"))(repository) )
-//
-//    println( ParkingServiceObj.park(Vehicle("AA122"))(repository) )
 
   }
 
