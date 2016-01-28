5bf1ec08c9de813015c1101e6284487af3b6f8b0
ddd-fun <laserdev@gmail.com>
2016-01-29 00:12:02 +0100

    fixed bug with taken lot

--- src/main/scala/org/garage/guru/infrastructure/InMemoryRepository.scala
+++ src/main/scala/org/garage/guru/infrastructure/InMemoryRepository.scala
@@ -12,11 +12,13 @@ class InMemoryRepository extends Repository{
 
 
   override def findFreeLot(): Try[FreeParkingLot] = {
-    repo.values.find(_.isInstanceOf[FreeParkingLot]).map(fl => Success(fl.asInstanceOf[FreeParkingLot])).getOrElse(Failure(new RuntimeException("lot is not found")))
+    repo.values.find(_.isInstanceOf[FreeParkingLot]).map(fl => Success(fl.asInstanceOf[FreeParkingLot])).getOrElse(Failure(new RuntimeException("free lot is not found")))
   }
 
   override def findTakenLot(vehicle: Vehicle): Try[TakenParkingLot] = {
-    repo.values.find(_.isInstanceOf[TakenParkingLot]).map(fl => Success(fl.asInstanceOf[TakenParkingLot])).getOrElse(Failure(new RuntimeException("lot is not found")))
+    repo.values.find(_ match {case TakenParkingLot(_, Vehicle(vehicle.licensePlate)) => true
+                              case _=> false} )
+      .map(fl => Success(fl.asInstanceOf[TakenParkingLot])).getOrElse(Failure(new RuntimeException("lot taken by "+vehicle+" is not found")))
   }
 
   override def save[L <: ParkingLot](parkingLot: L): Try[L] = {
