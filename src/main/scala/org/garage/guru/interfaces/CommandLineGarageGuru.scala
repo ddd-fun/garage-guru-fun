c41aed7f15be9b593d680e50d861dfae4363b60e
ddd-fun <laserdev@gmail.com>
2016-02-01 23:30:48 +0100

    clean command line

--- src/main/scala/org/garage/guru/interfaces/CommandLineGarageGuru.scala
+++ src/main/scala/org/garage/guru/interfaces/CommandLineGarageGuru.scala
@@ -2,31 +2,25 @@ package org.garage.guru.interfaces
 
 import org.garage.guru.application.ParkingService
 import org.garage.guru.domain._
+import org.garage.guru.infrastructure.InMemoryRepository
 
 object CommandLineGarageGuru  {
 
   def main(args: Array[String]) {
 
 
-    val cs =  CarSpec
-
-    val orspec =  cs.or(MotorbikeSpec)
-
-    //println(CarSpec or MotorbikeSpec isSatisfiedBy Motorbike("sagfei") )
-
-   println( "(\\w*)\\s*(\\w*)\\s*(\\w*)".r.findFirstMatchIn("park car"))
-
-
-    import org.garage.guru.infrastructure.InMemoryRepository
-    val repository = new InMemoryRepository
-    repository.addFreeLot(FreeParkingLot(LotLocation("A", "1"), CarSpec or MotorbikeSpec))
+    Repository.addFreeLot(FreeParkingLot(LotLocation("A", "1"), CarSpec or MotorbikeSpec))
 
     for (ln <- io.Source.stdin.getLines){
       "(\\w*)\\s*(\\w*)\\s*(\\w*)".r.findFirstMatchIn(ln).map(_.subgroups.filter(!_.isEmpty)) match {
         case Some(List("exit")) => scala.util.control.Breaks.break();
-        case Some(List("free")) => println( ParkingServiceObj.freeLots()(repository) );
-        case Some(List("park", t, v)) =>  Vehicle(t,v) match { case Some(v) => println( ParkingServiceObj.park(v)(repository) ) case _=> println("unknown command "+ln) }
-        case Some(List("clean", t, v)) => Vehicle(t,v) match { case Some(v) => println( ParkingServiceObj.cleanParkingLot(v)(repository) ) case _=> println("unknown command "+ln) }
+        case Some(List("free")) => println( ParkingServiceObj.freeLots()(Repository) );
+        case Some(List("park", t, v)) =>  Vehicle(t,v) match {
+               case Some(v) => println( ParkingServiceObj.park(v)(Repository) )
+               case _=> println("unknown command "+ln) }
+        case Some(List("clean", t, v)) => Vehicle(t,v) match {
+               case Some(v) => println( ParkingServiceObj.cleanParkingLot(v)(Repository) )
+               case _=> println("unknown command "+ln) }
         case _=> println("unknown command "+ln)
       }
     }
@@ -36,6 +30,6 @@ object CommandLineGarageGuru  {
 
   object ParkingServiceObj extends ParkingService
 
-
+  object Repository extends InMemoryRepository
 
 }
