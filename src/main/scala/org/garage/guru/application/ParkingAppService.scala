e63f098ca36d7466d1853d7252dae80b54ca2083
ddd-fun <laserdev@gmail.com>
2016-05-06 17:58:41 +0200

    domain service dependency as reader

--- src/main/scala/org/garage/guru/application/ParkingAppService.scala
+++ src/main/scala/org/garage/guru/application/ParkingAppService.scala
@@ -4,7 +4,7 @@ package org.garage.guru.application
 import org.garage.guru.domain._
 
 import scala.util.Try
-import scalaz.Bind
+import scalaz.{KleisliFunctions, KleisliInstances, ReaderT, Bind}
 
 
 trait ParkingAppService {
@@ -13,19 +13,29 @@ trait ParkingAppService {
 
   type Repo =  Repository[FreeParkingLot, TakenParkingLot, Vehicle, VehicleId]
 
-  def findParkedVehicle(vehicleId: VehicleId)(service: DomainService) = {
-     service.findParkedVehicle(vehicleId).map(_.lotLocation)
+  type RepoInj[A] = ReaderT[Try, Repo, A]
+
+  type DomainServiceInj[A] = ReaderT[RepoInj, DomainService, A]
+
+
+  def findParkedVehicle(vehicleId: VehicleId) : DomainServiceInj[LotLocation] = {
+     DomainServiceInj{_.findParkedVehicle(vehicleId).map(_.lotLocation)}
   }
 
   def freeLots(repo: Repo) = repo.freeLots()
 
-  def parkVehicle(vehicle: Vehicle)(service: DomainService) = {
-     service.parkVehicle(vehicle).map(_.lotLocation)
+  def parkVehicle(vehicle: Vehicle): DomainServiceInj[LotLocation] = {
+     DomainServiceInj{_.parkVehicle(vehicle).map(_.lotLocation)}
   }
 
-  def takeAwayVehicle(vehicleId: VehicleId)(service: DomainService) = {
-     service.takeAwayVehicle(vehicleId).map(_.lotLocation)
+  def takeAwayVehicle(vehicleId: VehicleId) : DomainServiceInj[LotLocation]= {
+    DomainServiceInj{_.takeAwayVehicle(vehicleId).map(_.lotLocation)}
   }
+
+  object DomainServiceInj extends KleisliInstances with KleisliFunctions {
+    def apply[A](f: DomainService => RepoInj[A]): DomainServiceInj[A] = kleisli(f)
+  }
+
 
   implicit val TryBind = new Bind[Try] {
     override def bind[A, B](fa: Try[A])(f: (A) => Try[B]): Try[B] = fa.flatMap(f)
