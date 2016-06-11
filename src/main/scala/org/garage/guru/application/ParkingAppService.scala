2d1225699b9715d93ee5518d535bebb71e803b48
ddd-fun <laserdev@gmail.com>
2016-06-12 00:44:36 +0200

    changed to type alias

--- src/main/scala/org/garage/guru/application/ParkingAppService.scala
+++ src/main/scala/org/garage/guru/application/ParkingAppService.scala
@@ -6,19 +6,19 @@ import org.garage.guru.domain._
 
 trait ParkingAppService {
 
-  def findParkedVehicle(vehicleId: VehicleId) : TryRepoAction[LotLocation] = {
+  def findParkedVehicle(vehicleId: VehicleId) : ParkingAction[LotLocation] = {
     ParkingServiceInterpreter.findTakenLot(vehicleId).map(_.lotLocation)
   }
 
-  def parkVehicle(vehicle: Vehicle): TryRepoAction[LotLocation] = {
+  def parkVehicle(vehicle: Vehicle): ParkingAction[LotLocation] = {
     ParkingServiceInterpreter.parkVehicle(vehicle).map(_.lotLocation)
   }
 
-  def takeAwayVehicle(vehicleId: VehicleId) : TryRepoAction[LotLocation]= {
+  def takeAwayVehicle(vehicleId: VehicleId) : ParkingAction[LotLocation]= {
     ParkingServiceInterpreter.takeAwayVehicle(vehicleId).map(_.lotLocation)
   }
 
-  def freeLots(): TryRepoAction[FreeParkingLots] = Repository.freeLots()
+  def freeLots(): ParkingAction[FreeParkingLots] = Repository.freeLots()
 
 
 }
